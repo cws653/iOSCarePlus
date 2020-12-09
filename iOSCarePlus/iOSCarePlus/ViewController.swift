5984f0d460630a312a30be18d36f6cdeb5f34384
최원석 <cws653@naver.com>
2020-12-09 23:57:16 +0900

    2주차
    뷰컨트롤러 애니메이션 기능 추가
    탭바 컨트롤러 추가
    LaunchScreen 만들기
    오토레이아웃 개념 공부 및 적용
    Swiftlint 적용

--- iOSCarePlus/iOSCarePlus/ViewController.swift
+++ iOSCarePlus/iOSCarePlus/ViewController.swift
@@ -8,12 +8,57 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var logoView: UIView!
+    @IBOutlet weak var logoViewTopConstraint: NSLayoutConstraint!
+    @IBOutlet weak var backgroundImageViewLeadingConstraint: NSLayoutConstraint!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        logoView.layer.cornerRadius = 15
+        
+        
     }
-
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        animationSettingDefault()
+        
+        appearLogoViewAnimation() { [weak self] in
+            self?.slideBackgroundImageAnimation()
+            self?.blinkLogoAnimation()
+        }
+       
+    }
+    
+    private func animationSettingDefault() {
+        logoViewTopConstraint.constant = -200
+        backgroundImageViewLeadingConstraint.constant = 0
+        logoView.alpha = 1
+        view.layoutIfNeeded()
+    }
+    
+    private func appearLogoViewAnimation(completion:@escaping () -> Void) {
+        UIView.animate(withDuration: 0.7, delay: 1, usingSpringWithDamping: 0.6, initialSpringVelocity: 1, options: []) { [weak self] in
+            self?.logoViewTopConstraint.constant = 17
+            self?.view.layoutIfNeeded()
+        } completion: { _ in
+            completion()
+        }
+    }
+    
+    private func slideBackgroundImageAnimation() {
+        UIView.animate(withDuration: 5, delay: 0, options: [.curveEaseInOut, .repeat, .autoreverse]) { [weak self] in
+            self?.backgroundImageViewLeadingConstraint.constant = -800
+            self?.view.layoutIfNeeded()
+        }
+    }
+    
+    private func blinkLogoAnimation() {
+        UIView.animateKeyframes(withDuration: 1, delay: 1, options: [.repeat, .autoreverse]) { [weak self] in
+            self?.logoView.alpha = 0
+            
+        }
+    }
 }
 
